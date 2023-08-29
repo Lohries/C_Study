725bd7001059076c215c3c136f97a726888a8ce7
Luis Henrique <129761489+Lohries@users.noreply.github.com>
2023-08-29 17:48:20 -0300

    Add files via upload

---
+++ Ponteiros/Lista2_Ex3.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main() {
+    int  mat1[100], mat2[100], contador = 0, *prt;
+
+    prt = mat;
+
+    for (i = 0; x > i; i++){
+        printf("Qual elemento sera inserido: ");
+        scanf("%i", prt);
+        printf("elemento %i: %i memeoria: %i ", contador, *prt, prt);
+        prt++;
+        contador++;
+        printf("Voce quer continuar: ");
+        scanf("%s", &confirm);
+    }
+    while(confirm == 'S');
+    return 0;
+
+}
